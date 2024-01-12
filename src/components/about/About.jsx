1c48cfa97ae33db97c0e4a45a2e3a7158e446f82
Rupeshsutarofficial <rupeshsutarofficial@gmail.com>
2024-01-12 10:57:19 +0530

    Resume Update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/about.jpg"
-import CV from "../../assets/Rupesh-Sutar-Resume.pdf"
+import CV from "../../assets/Rupeshsutar.pdf"
 import Info from './Info'
 
 const About = () => {
