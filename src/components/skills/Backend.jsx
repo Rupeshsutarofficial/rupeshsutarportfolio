54ae5c38d950a73f6f06f724b413df2a58ff53b4
Rupesh Sanjiv Sutar <146075201+Rupeshsutarofficial@users.noreply.github.com>
2024-01-27 12:36:15 +0530

    new icon added

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,78 +1,103 @@
-import React from 'react'
+import React from "react";
 
 const Backend = () => {
   return (
     <div className="skills__content">
-    <h3 className="skills__title">Backend Developer</h3>
-    <div className="skills__box">
-      <div className="skills__group">
-        <div className="skills__data">
-          <i className="bx bxl-java"></i>
-          <div>
-            <h3 className="skills__name">Java</h3>
-            <span className="skills__level">Intermediate</span>
+      <h3 className="skills__title">Backend Developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bxl-java"></i>
+            <div>
+              <h3 className="skills__name">Java</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="currentColor"
+                d="M17.193 9.555c-1.264-5.58-4.252-7.414-4.573-8.115c-.28-.394-.53-.954-.735-1.44c-.036.495-.055.685-.523 1.184c-.723.566-4.438 3.682-4.74 10.02c-.282 5.912 4.27 9.435 4.888 9.884l.07.05A73.49 73.49 0 0 1 11.91 24h.481a28.48 28.48 0 0 1 .51-3.07c.417-.296.604-.463.85-.693a11.342 11.342 0 0 0 3.639-8.464c.01-.814-.103-1.662-.197-2.218m-5.336 8.195s0-8.291.275-8.29c.213 0 .49 10.695.49 10.695c-.381-.045-.765-1.76-.765-2.405"
+              />
+            </svg>
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className="bx bxl-python"></i>
+            <div>
+              <h3 className="skills__name">Python</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bxl-spring-boot"></i>
+            <div>
+              <h3 className="skills__name">springboot</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
           </div>
         </div>
 
-        <div className="skills__data">
-          <i className="bx bxl-nodejs"></i>
-          <div>
-            <h3 className="skills__name">NodeJS</h3>
-            <span className="skills__level">Intermediate</span>
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bxs-data"></i>
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
           </div>
-        </div>
 
-        <div className="skills__data">
-          <i className="bx bxl-python"></i>
-          <div>
-            <h3 className="skills__name">Python</h3>
-            <span className="skills__level">Intermediate</span>
+          <div className="skills__data">
+            <i className="bx bxl-nodejs"></i>
+            <div>
+              <h3 className="skills__name">NodeJS</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
           </div>
-        </div>
-        <div className="skills__data">
-          <i className="bx bxl-spring-boot"></i>
-          <div>
-            <h3 className="skills__name">springboot</h3>
-            <span className="skills__level">Intermediate</span>
-          </div>
-        </div>
-      </div>
 
-      <div className="skills__group">
-        <div className="skills__data">
-          <i className="bx bxs-data"></i>
-          <div>
-            <h3 className="skills__name">MySQL</h3>
-            <span className="skills__level">Intermediate</span>
+          <div className="skills__data">
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 256 256"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g fill="none">
+                <rect width="256" height="256" fill="#F4F2ED" rx="60" />
+                <path
+                  fill="#0F0F0F"
+                  d="M228 182.937a12.732 12.732 0 0 1-15.791-6.005c-9.063-13.567-19.071-26.522-28.69-39.755l-4.171-5.56c-11.454 15.346-22.908 30.08-33.361 45.371a12.23 12.23 0 0 1-15.012 5.894l42.98-57.659l-39.978-52.1a13.289 13.289 0 0 1 15.847 5.56c9.285 13.568 19.572 26.523 29.802 40.257c10.287-13.623 20.462-26.634 29.97-40.09a11.952 11.952 0 0 1 14.901-5.56l-15.513 20.573c-6.95 9.174-13.789 18.404-21.017 27.356a5.558 5.558 0 0 0 0 8.285c13.289 17.626 26.466 35.307 40.033 53.433M28 124.5c1.168-5.56 1.89-11.621 3.503-17.292c9.619-34.195 48.818-48.43 75.785-27.245c15.791 12.4 19.739 29.97 18.961 49.764H37.286c-1.446 35.363 24.075 56.714 56.713 45.816a33.864 33.864 0 0 0 21.518-23.965c1.724-5.56 4.504-6.505 9.786-4.893a45.145 45.145 0 0 1-21.573 32.972a52.263 52.263 0 0 1-60.884-7.784a54.767 54.767 0 0 1-13.678-32.138c0-1.89-.723-3.781-1.112-5.56A860.69 860.69 0 0 1 28 124.5m9.397-2.391h80.456c-.501-25.632-16.681-43.814-38.254-43.98c-24.02-.334-41.201 17.458-42.258 43.869z"
+                />
+              </g>
+            </svg>
+            <div>
+              <h3 className="skills__name">ExpressJS</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
           </div>
-        </div>
-
-        <div className="skills__data">
-        <i class="bx bx-badge-check"></i>
-          <div>
-            <h3 className="skills__name">MongoDB</h3>
-            <span className="skills__level">Intermediate</span>
-          </div>
-        </div>
-
-        <div className="skills__data">
-          <i className="bx bx-badge-check"></i>
-          <div>
-            <h3 className="skills__name">ExpressJS</h3>
-            <span className="skills__level">Intermediate</span>
-          </div>
-        </div>
-        <div className="skills__data">
-          <i className="bx bx-badge-check"></i>
-          <div>
-            <h3 className="skills__name">Hibernate</h3>
-            <span className="skills__level">Intermediate</span>
+          <div className="skills__data">
+          <svg width="15" height="15" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path fill="currentColor" d="M5.365 0L9.98 7.994h8.95L14.31 0zm-.431.248L.46 7.994l4.613 8.008L9.55 8.24zm13.992 7.75l-4.475 7.76l4.617 7.992l4.471-7.744zm-4.905 8.006l-8.95.002L9.688 24h8.946l-4.615-7.994l.001-.002Z"/>
+</svg>
+            <div>
+              <h3 className="skills__name">Hibernate</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Backend
+export default Backend;
--- src/components/skills/Otherskills.jsx
+++ src/components/skills/Otherskills.jsx
@@ -14,7 +14,9 @@ const Otherskills = () => {
           </div>
         </div>
         <div className="skills__data">
-        <i class='bx bxs-video'></i>
+        <svg width="15" height="15" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path fill="currentColor" d="m4 3l2 4h3L7 3h2l2 4h3l-2-4h2l2 4h3l-2-4h3q.825 0 1.413.588T22 5v14q0 .825-.587 1.413T20 21H4q-.825 0-1.412-.587T2 19V5q0-.825.588-1.412T4 3m4 15h8v-.55q0-1.1-1.1-1.775T12 15q-1.8 0-2.9.675T8 17.45zm4-4q.825 0 1.413-.587T14 12q0-.825-.587-1.412T12 10q-.825 0-1.412.588T10 12q0 .825.588 1.413T12 14"/>
+</svg>
           <div>
             <h3 className="skills__name">Cinematic video Editing</h3>
             <span className="skills__level">Advanced</span>
@@ -28,7 +30,10 @@ const Otherskills = () => {
           </div>
         </div>
         <div className="skills__data">
-        <i class='bx bxs-color' ></i>
+        <svg width="15" height="15" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <path fill="currentColor" d="M3 2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm0 15a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1z"/>
+    <path fill="currentColor" d="M17 4H3v10h14zM5 12l2.5-3l2 2L12 8l3 4zm-1 3h12v1H4z"/>
+</svg>
           <div>
             <h3 className="skills__name">Youtube Thumbnail Desginer</h3>
             <span className="skills__level">Advanced</span>
