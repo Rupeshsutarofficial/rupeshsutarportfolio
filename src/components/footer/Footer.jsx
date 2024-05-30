4f8295c46a4621120013d28d66d1418e45fde09a
Rupesh Sanjiv Sutar <146075201+Rupeshsutarofficial@users.noreply.github.com>
2024-05-30 17:23:41 +0530

    Youtube link and icon Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,53 +5,80 @@ const Footer = () => {
   const isMobile = window.innerWidth <= 768; // Adjust the width as needed
 
   return (
-    <footer className="footer">
-      <div className="footer__container container">
-        <h1 className="footer__title">Rupesh Sutar</h1>
-        <ul className="footer__list">
+    <footer className='footer'>
+      <div className='footer__container container'>
+        <h1 className='footer__title'>Rupesh Sutar</h1>
+        <ul className='footer__list'>
           <li>
-            <a href="#about" className={`footer__link ${!isMobile ? 'magnet-target' : ''}`}>
+            <a
+              href='#about'
+              className={`footer__link ${!isMobile ? "magnet-target" : ""}`}
+            >
               About
             </a>
           </li>
           <li>
-            <a href="#portfolio" className={`footer__link ${!isMobile ? 'magnet-target' : ''}`}>
+            <a
+              href='#portfolio'
+              className={`footer__link ${!isMobile ? "magnet-target" : ""}`}
+            >
               Projects
             </a>
           </li>
           <li>
-            <a href="#testimonials" className={`footer__link ${!isMobile ? 'magnet-target' : ''}`}>
+            <a
+              href='#testimonials'
+              className={`footer__link ${!isMobile ? "magnet-target" : ""}`}
+            >
               Testimonials
             </a>
           </li>
         </ul>
-        <div className="footer__social">
+        <div className='footer__social'>
           <a
-            href="https://www.instagram.com/mr.rupeshsutar/"
-            className={`footer__social-link ${!isMobile ? 'magnet-target' : ''}`}
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.instagram.com/mr.rupeshsutar/'
+            className={`footer__social-link ${
+              !isMobile ? "magnet-target" : ""
+            }`}
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <i className="bx bxl-instagram"></i>
+            <i className='bx bxl-instagram'></i>
           </a>
           <a
-            href="https://www.linkedin.com/in/rupeshsutarofficial/"
-            className={`footer__social-link ${!isMobile ? 'magnet-target' : ''}`}
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.linkedin.com/in/rupeshsutarofficial/'
+            className={`footer__social-link ${
+              !isMobile ? "magnet-target" : ""
+            }`}
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <i className="bx bxl-linkedin"></i>
+            <i className='bx bxl-linkedin'></i>
           </a>
           <a
-            href="https://github.com/Rupeshsutarofficial"
-            className={`footer__social-link ${!isMobile ? 'magnet-target' : ''}`}
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://github.com/Rupeshsutarofficial'
+            className={`footer__social-link ${
+              !isMobile ? "magnet-target" : ""
+            }`}
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <i className="bx bxl-github"></i>
+            <i className='bx bxl-github'></i>
+          </a>
+          <a
+            href='https://www.youtube.com/@Shubhfilmandedit'
+            className={`footer__social-link ${
+              !isMobile ? "magnet-target" : ""
+            }`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <i class='bx bxl-youtube'></i>
           </a>
         </div>
-        <span className="footer__copy">&#169; RupeshSutar. All rights reserved</span>
+        <span className='footer__copy'>
+          &#169; RupeshSutar. All rights reserved
+        </span>
       </div>
     </footer>
   );
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,30 +3,38 @@ import React from "react";
 const Social = () => {
   const isMobile = window.innerWidth <= 768;
   return (
-    <div className="home__social">
+    <div className='home__social'>
       <a
-        href="https://www.instagram.com/mr.rupeshsutar/"
-        className={`home__social-icon ${!isMobile ? 'magnet-target' : ''}`}
-        target="_blank"
-        rel="noopener noreferrer"
+        href='https://www.instagram.com/mr.rupeshsutar/'
+        className={`home__social-icon ${!isMobile ? "magnet-target" : ""}`}
+        target='_blank'
+        rel='noopener noreferrer'
       >
-        <i className="uil uil-instagram-alt"></i>
+        <i className='uil uil-instagram-alt'></i>
       </a>
       <a
-        href="https://www.linkedin.com/in/rupeshsutarofficial/"
-        className={`home__social-icon ${!isMobile ? 'magnet-target' : ''}`}
-        target="_blank"
-        rel="noopener noreferrer"
+        href='https://www.linkedin.com/in/rupeshsutarofficial/'
+        className={`home__social-icon ${!isMobile ? "magnet-target" : ""}`}
+        target='_blank'
+        rel='noopener noreferrer'
       >
-        <i className="uil uil-linkedin"></i>
+        <i className='uil uil-linkedin'></i>
       </a>
       <a
-        href="https://github.com/Rupeshsutarofficial"
-        className={`home__social-icon ${!isMobile ? 'magnet-target' : ''}`}
-        target="_blank"
-        rel="noopener noreferrer"
+        href='https://github.com/Rupeshsutarofficial'
+        className={`home__social-icon ${!isMobile ? "magnet-target" : ""}`}
+        target='_blank'
+        rel='noopener noreferrer'
       >
-        <i className="uil uil-github"></i>
+        <i className='uil uil-github'></i>
+      </a>
+      <a
+        href='https://www.youtube.com/@Shubhfilmandedit'
+        className={`home__social-icon ${!isMobile ? "magnet-target" : ""}`}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <i class='bx bxl-youtube'></i>
       </a>
     </div>
   );
